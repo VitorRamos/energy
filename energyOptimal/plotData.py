ee383277ae58eb6895acad1ad50394efcd9eaf2e
vitorramos <ramos.vitor89@gmail.com>
2018-09-11 18:35:28 -0300

    add sabe plot function

--- energyOptimal/plotData.py
+++ energyOptimal/plotData.py
@@ -4,6 +4,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
+legends= []
 
 def setProps(xlabel='', ylabel='', zlabel='', title=''):
     plt.title(title)
@@ -11,18 +12,18 @@ def setProps(xlabel='', ylabel='', zlabel='', title=''):
     ax.set_ylabel(ylabel, fontsize=10)
     ax.set_zlabel(zlabel, fontsize=10)
 
-def plot3D(x, y, z, points=True):
-    z= np.reshape(z, (len(x), len(y)))
+def plot3D(x, y, z, legend='', points=True):
 
+    z= np.reshape(z, (len(x), len(y)))
     X = np.arange(0, len(x), 1)
     Y = np.arange(0, len(y), 1)
     X, Y = np.meshgrid(X, Y)
     Z = z[X, Y]
 
     if points:
-        surf = ax.scatter(X, Y, Z, antialiased=True, color='r')
+        surf = ax.scatter(X, Y, Z, antialiased=True, color='b')
     else:
-        surf = ax.plot_wireframe(X, Y, Z, antialiased=True)
+        surf = ax.plot_wireframe(X, Y, Z, antialiased=True, color='black')
     
     ax.set_xticks(np.arange(0, len(x), 2))
     ax.set_xticklabels(x[::2], fontsize=10)
@@ -30,5 +31,13 @@ def plot3D(x, y, z, points=True):
     ax.set_yticks(np.arange(0, len(y), 2))
     ax.set_yticklabels(y[::2], fontsize=10)
 
-def plotShow():
+    legends.append(legend)
+
+def plotShow(showLegend= False):
+    if showLegend: plt.legend(legends)
     plt.show()
+
+def savePlot(filename, showLegend= False):
+    if showLegend: plt.legend(legends)
+    plt.tight_layout()
+    plt.savefig(filename)
