1399029455894fa50036f9d96a963f55dcb4fd66
vitorramos <ramos.vitor89@gmail.com>
2018-09-12 14:41:51 -0300

    unsed variable

--- energyOptimal/plotData.py
+++ energyOptimal/plotData.py
@@ -48,9 +48,9 @@ def plot3D(x, y, z, legend='', points=True):
     X, Y = np.meshgrid(x, y)
 
     if points:
-        surf = ax.scatter(X, Y, Z, antialiased=True, color='b')
+        ax.scatter(X, Y, Z, antialiased=True, color='b')
     else:
-        surf = ax.plot_wireframe(X, Y, Z, antialiased=True, color='black')
+        ax.plot_wireframe(X, Y, Z, antialiased=True, color='black')
     
     ax.set_xticks(x[::2])#np.arange(0, len(x), 2))
     ax.set_xticklabels(x[::2], fontsize=10)
