46110010b4d0736d7da364a58f36e014390a6983
vitorramos <ramos.vitor89@gmail.com>
2018-09-28 22:47:38 -0300

    load as dataframe

--- energyOptimal/performanceModel.py
+++ energyOptimal/performanceModel.py
@@ -16,9 +16,11 @@ class performanceModel:
             self.loadDataFrame(dataFramefile)
             self.loadSVR(svrfile)
 
-    def loadData(self, filename, arg_num, verbose=0, method= 'constTime', createDataFrame= True,
+
+    def loadData_(self, filename, arg_num, verbose=0, method= 'constTime', createDataFrame= True,
                  freqs_filter=[], thrs_filter=[]):
         '''
+        deprecated
         filename: string
         arg_num: argument that contains the problem size
         verbose: number, level of verbose mode
@@ -30,7 +32,6 @@ class performanceModel:
         createDataFrame: boolean
         freqs_filter: list, list of frequencies to load
         thrs_filter: list, list of threads to load
-
         return dataFrame if createDataFrame is True otherwise frequencies, threads, powers
         '''
 
@@ -80,13 +81,18 @@ class performanceModel:
                     elif has_ipmi:
                         if method == 'timeDiff':
                             pw= 0
+                            print(freq,thr['nthread'], p['arg'][arg_num])
                             for i in range(len(p['ipmi'])):
                                 pot = p['ipmi'][i]['sensor']['sources'][0]['dcOutPower']
                                 pot += p['ipmi'][i]['sensor']['sources'][1]['dcOutPower']
                                 if i - 1 >= 0:
                                     pw += (p['ipmi'][i]['time']-p['ipmi'][i - 1]['time'])*pot
+                                    print((p['ipmi'][i]['time']-p['ipmi'][i - 1]['time'])*pot)
                                 if i == len(p['ipmi']) - 1:
                                     pw += pot * (p['total_time']-p['ipmi'][i]['time'])
+                                    print(pot * (p['total_time']-p['ipmi'][i]['time']),
+                                            (p['total_time']-p['ipmi'][i]['time']))
+                            print('-'*100)
 
                         else:    
                             for s in p['ipmi']:
@@ -127,7 +133,98 @@ class performanceModel:
             return self.dataFrame
         
         return self.frequencies, self.threads, self.powers
-    
+
+    def loadData(self, filename, arg_num, verbose=0, method= 'constTime',
+                        freqs_filter=[], thrs_filter=[]):
+        '''
+        filename: string
+        arg_num: argument that contains the problem size
+        verbose: number, level of verbose mode #TODO
+        method: string
+            constTime : Average the power samples to estimate energy,
+                        this assume the interval between samples are constant
+            timeDiff : Uses time interval to estimate energy and accumulates
+            allSamples : Keep all samples on the power array, energy it's not estimated
+        freqs_filter: list, list of frequencies to load
+        thrs_filter: list, list of threads to load
+
+        return dataFrame if createDataFrame is True otherwise frequencies, threads, powers
+        '''
+
+        with open(filename,'rb+') as f:
+            data= pickle.load(f)
+
+        has_ipmi= 'ipmi' in data[0]['threads'][0]['lpcpu'][0].keys()
+        has_rapl= 'rapl' in data[0]['threads'][0]['lpcpu'][0].keys()
+
+        for thr in data[0]['threads']:
+            if len(thrs_filter) > 0 and thr['nthreads'] not in thrs_filter: continue
+            self.threads.append(thr['nthread'])
+
+        df = []
+        for d in data:
+            assert len(d['threads']) == len(self.threads)
+            if len(freqs_filter) > 0 and freq not in freqs_filter: continue
+            for thr in d['threads']:
+                if len(thrs_filter) > 0 and thr['nthreads'] not in thrs_filter: continue
+                for p in thr['lpcpu']:
+                    pw = 0
+                    pw_size = 0
+                    if has_rapl:
+                        for s in p['rapl']:
+                            aux= float(s['sensor']) 
+                            if aux > 0:
+                                pw+=aux
+                                pw_size+=1                                
+                                df.append([d['freq'], thr['nthread'], p['arg'][arg_num], 
+                                        s['time'], aux])
+
+                    elif has_ipmi:
+                        for s in p['ipmi']:
+                            pot = float(s['sensor']['sources'][0]['dcOutPower']+
+                                    s['sensor']['sources'][1]['dcOutPower'])
+                            pw+=pot
+                            df.append([d['freq'], thr['nthread'], p['arg'][arg_num],
+                                        s['time'], pot])
+                        pw_size= len(p['ipmi'])
+                     
+                    df.append([d['freq'], thr['nthread'], p['arg'][arg_num], 
+                        p['total_time'], pw/pw_size])
+
+                    if verbose > 0:
+                        print(d['freq'], thr['nthread'], p['arg'][arg_num], 
+                                'T', p['total_time'], 'P', pw, 'E', p['total_time']*pw)
+
+        self.dataFrame = pd.DataFrame(df, columns=['freq', 'thr', 'in', 'time', 'pw'])
+        cat = pd.factorize(self.dataFrame['in'])
+        self.dataFrame['in_cat'] = cat[0] + 1
+        self.dataFrame['freq'] = self.dataFrame['freq'].astype(float)/1e6
+        self.dataFrame= self.dataFrame.sort_values(['freq','thr','in_cat','time'])
+
+        if method == 'constTime':
+            df = self.dataFrame.groupby(['freq','thr','in_cat']).mean().reset_index()
+            df['time'] = self.dataFrame.groupby(['freq','thr', 'in_cat']).tail(1)['time'].values
+
+            self.dataFrame = df
+            self.dataFrame['energy'] = self.dataFrame['time']*self.dataFrame['pw']
+
+        elif method == 'timeDiff':
+            def difftime(df):
+                df['time_diff'] = np.hstack(([0], df['time'].values[1:]-df['time'].values[:-1]))
+                df['energy']= df['time_diff']*df['pw']
+                return df
+            self.dataFrame= self.dataFrame.groupby(['freq','thr','in_cat']).apply(difftime)
+            df = self.dataFrame.groupby(['freq','thr','in_cat']).mean().reset_index()
+            df['time'] = self.dataFrame.groupby(['freq','thr','in_cat']).tail(1)['time'].values
+            df['energy'] = self.dataFrame.groupby(['freq','thr','in_cat']).energy.sum().values
+            self.dataFrame = df.drop(columns='time_diff')
+        
+        self.threads= self.dataFrame['thr'].unique()
+        self.frequencies= self.dataFrame['freq'].unique()
+        self.power= self.dataFrame['pw'].values
+
+        return self.dataFrame
+
     def saveDataframe(self, filename):
         assert self.dataFrame is not None
         with open(filename, 'wb+') as f:
@@ -144,7 +241,7 @@ class performanceModel:
 
         C_: svr parameter
         gamma_: svr parameter
-        test_size: percentage of samples used on test
+        test_size: percentage of samples used on test #TODO
 
         return svr
         '''
@@ -199,13 +296,13 @@ class performanceModel:
     def estimate(self, X):
         '''
         Estimate the time from the list of [freq, thr, in]
-        
+
         return list of estimatives
         '''
         assert self.svr is not None
         Y= self.svr.predict(X)
         return Y
-    
+
 
     def error(self, method='mab'):
         '''
