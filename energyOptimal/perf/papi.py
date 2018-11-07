0e667b12b351b91960a7f31654c2f767544590f8
= <=>
2018-11-07 16:18:41 +0100

    repeat

--- energyOptimal/perf/papi.py
+++ energyOptimal/perf/papi.py
@@ -1,20 +1,41 @@
 from pypapi import papi_low as papi
 from pypapi import events
 import subprocess, os, signal
+import numpy as np
+import time
 
 papi.library_init()
-evs= papi.create_eventset()
-papi.add_event(evs, events.PAPI_TOT_INS)
+total=[]
+cont= 0
 
-p= subprocess.Popen(['./teste'])
-os.kill(p.pid, signal.SIGSTOP)
-papi.attach(evs, p.pid)
-os.kill(p.pid, signal.SIGCONT)
+for i in range(100):
+    try:
+        evs= papi.create_eventset()
+        papi.add_event(evs, events.PAPI_TOT_INS)
+        papi.add_event(evs, events.PAPI_BR_INS)
+        papi.add_event(evs, events.PAPI_BR_MSP)
+        papi.add_event(evs, events.PAPI_L3_TCR)
 
-papi.start(evs)
-p.wait()
-result= papi.stop(evs)
-print(result)
+        p= subprocess.Popen(['./teste'], stdout= subprocess.PIPE)
+        os.kill(p.pid, signal.SIGSTOP)
+        papi.attach(evs, p.pid)
+        papi.start(evs)
+        os.kill(p.pid, signal.SIGCONT)
+        p.wait()
+        result= papi.stop(evs)
+        total.append(result)
+        cont+=1
+        papi.cleanup_eventset(evs)
+        papi.destroy_eventset(evs)
+        # print("Not Too fast", i)
+        # print(result)
+    except Exception as e:
+        print("Too fast", cont)
+        print(e)
+        pass
 
-papi.cleanup_eventset(evs)
-papi.destroy_eventset(evs)
+print(cont)
+total= np.asarray(total)
+print(total.mean(axis=0))
+print(total.std(axis=0))
+
