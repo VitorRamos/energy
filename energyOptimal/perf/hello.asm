7035fa8c9f75eb0a42a72c1985a55379003911e7
= <=>
2018-11-07 16:17:29 +0100

    loop size

--- energyOptimal/perf/hello.asm
+++ energyOptimal/perf/hello.asm
@@ -4,22 +4,23 @@ section .data
 section .text
     global _start
 _start:
-    push    rbp
-    mov     rbp, rsp
+    push    rbp 
+    mov     rbp, rsp 
     mov     DWORD[rbp-0x4], 0x0
 
     loop:
     mov     rax, 1
     mov     rdi, 1
-    mov     rsi, msg
+    mov     rsi, msg 
     mov     rdx, 13
     syscall
     add     DWORD[rbp-0x4], 1
-    cmp     DWORD[rbp-0x4], 10
+    cmp     DWORD[rbp-0x4], 1000
     jne     loop
     
     pop rbp
-
     mov    rax, 60
     mov    rdi, 0
     syscall
+
+; 3 + 8*1000 + 4 = 8007 instructions
