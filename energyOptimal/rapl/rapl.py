4c37ea8841f639d710cafb8d9ed8761e7a25ea90
vitorramos <ramos.vitor89@gmail.com>
2019-04-03 13:27:44 +0200

    support different methods of reading rapl

--- energyOptimal/rapl/rapl.py
+++ energyOptimal/rapl/rapl.py
@@ -5,20 +5,34 @@ import os
 
 class RAPL:
 	RAPL_ENERGY_DIR= '/sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj'
+	PKG_ENERGY_SCALE= 2.3283064365386962890625e-10
 		
-	def __init__(self, name_):
-		if os.stat(RAPL.RAPL_ENERGY_DIR):
-			self.method= 'RAPL'
-		elif which('scontrol'):
-			self.method= 'SCONTROL'
-		elif which('perf'):
-			self.method= 'PERF'
+	def __init__(self, name_, method=None):
+
+		if method == None:
+			if os.stat(RAPL.RAPL_ENERGY_DIR):
+				self.method= 'RAPL'
+			elif which('scontrol'):
+				self.method= 'SCONTROL'
+			else:
+				self.method= 'PERF'
 		else:
-			raise "Cant measure energy"
+			self.method= method
+		
+		if method == 'PERF':
+			import profiler
+			try:
+				self.prof= profiler.Profiler(events_groups=[["SYSTEMWIDE:RAPL_ENERGY_PKG"]])
+				self.prof.start_counters(0)
+			except:
+				raise "Cant measure energy"
+
 		self.name= name_
 		self.data= []
 		self.init_rapl= 0
 		self.end_rapl= 0
+		self.init_perf= 0
+		self.end_perf= 0
 	
 	def get_data_scontrol(self):
 		data= subprocess.check_output(["scontrol","show","node", self.name])
@@ -38,13 +52,28 @@ class RAPL:
 		self.data= val/1e6/0.1
 		return self.data
 	
+	def get_data_perf(self):
+		val= self.prof.read_events()[0]
+		time.sleep(0.1)
+		val= self.prof.read_events()[0]-val
+		self.data= val*RAPL.PKG_ENERGY_SCALE/0.1
+		return self.data
+	
 	def start_en_rapl(self):
 		self.init_rapl= float(open(RAPL.RAPL_ENERGY_DIR).read())
 		return self.init_rapl
 
 	def end_en_rapl(self):
 		self.end_rapl= float(open(RAPL.RAPL_ENERGY_DIR).read())
-		return self.end_rapl-self.init_rapl
+		return (self.end_rapl-self.init_rapl)/1e6
+
+	def start_en_perf(self):
+		self.init_perf= self.prof.read_events()[0]
+		return self.init_perf
+	
+	def end_en_perf(self):
+		self.end_perf= self.prof.read_events()[0]
+		return (self.end_perf-self.init_perf)*RAPL.PKG_ENERGY_SCALE
 
 	def get_data(self):
 		if self.method == 'RAPL':
@@ -52,7 +81,7 @@ class RAPL:
 		elif self.method == 'SCONTROL':
 			return self.get_data_scontrol()
 		elif self.method == 'PERF':
-			raise 'Not implemented'
+			return self.get_data_perf()
 
 	def print_data(self):
 		print("Power consumption", self.data)
